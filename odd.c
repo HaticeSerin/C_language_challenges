f10313332fc0babbe21d4d0923fe009205e11f17
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-22 20:37:01 +0300

    Hatice Serin
    @themanoftalent

--- odd.c
+++ odd.c
@@ -1,16 +1,33 @@
+//Lütfen geliştirip yazınız!
 #include <stdio.h>
 int main()
 {
-    int n,i;
-    printf("Enter the no. to check odd and even :");
-    scanf("%d",&n);
-    if (n%2==0)
-    {
-        printf("the no is the even :%d",n);
+    int sayi1,sayi2,i;
+    printf("Birinci sayiyi giriniz: ");
+    scanf("%d",&sayi1);
+    printf("Ikinci sayiyi giriniz: ");
+    scanf("%d",&sayi2);
+    printf("Cift sayilar Tek sayilar\n");
+    if(sayi1<sayi2){
+    for(i=sayi1;i<=sayi2;i++){
+    	if(i%2==0){
+    		printf("%d\n\t",i);
+		}
+		else{
+			printf("\t%d\n",i);
+		}
     }
-    else
-    {
-        printf("the no is odd :%d",n);
     }
-    
+    if(sayi2<sayi1){
+    	for(i=sayi2;i<=sayi1;i++){
+    	if(i%2==0){
+    		printf("%d\n\t",i);
+		}
+	    else{
+			printf("\t%d\n",i);
+		}
+    }
+}
+	
+    return 0;
 }
