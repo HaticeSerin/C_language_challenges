229bc2b186e62c1518157d68695cab1441960069
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-22 17:57:55 +0300

    Hatice Serin
    @themanoftalent

--- piramit.c
+++ piramit.c
@@ -1,14 +1,33 @@
-
+//Lütfen geliştirip yazınız!
 #include <stdio.h>
-int main() {
-    int i, j, kenar;
-    printf("piramit kenar sayısı: ");
-    scanf("%d", &kenar);
-    for (i = kenar; i >= 1; --i) {
-        for (j = 1; j <= i; ++j) {
-            printf("%d ", j);
+int main(){
+    int i,j,k,m,n;
+    printf("piramit satir sayisi: ");
+    scanf("%d",&n);
+    for (i=1;i<=n;i++){
+        for (j=1;j<=n-i;j++){
+            printf(" ");
         }
+        for(k=1;k<=i;k++){
+        	printf("%d",k);
+		}
+		for(m=i-1;m>=1;--m){
+        		printf("%d",m);
+			}
         printf("\n");
     }
+    for (i=n-1;i>=1;i--){
+        for (j=n-i;j>=1;j--){
+            printf(" ");
+        }
+        for(k=1;k<=i;k++){
+        	printf("%d",k);
+		}
+		for(m=i-1;m>=1;--m){
+      		printf("%d",m);
+		}
+        printf("\n");
+    }
+    
     return 0;
 }
